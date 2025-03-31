14b090ff71fb17d29e6c96370c72681332cf0aa9
Roma <voskovshchukr@bk.ru>
2025-03-31 21:57:05 +0300

    Мапа и обновление

--- main.go
+++ main.go
@@ -2,21 +2,29 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
 
 type Message struct {
+	ID   int    `json:"id"`
 	Text string `json:"text"`
 }
+
 type Response struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
 }
 
-var messages []Message
+var messages = make(map[int]Message)
+var nextID = 1
 
 func GetHandler(c echo.Context) error {
+	var messageMap []Message
+	for _, msg := range messages {
+		messageMap = append(messageMap, msg)
+	}
 	return c.JSON(http.StatusOK, &messages)
 }
 
@@ -29,33 +37,82 @@ func PostHandler(c echo.Context) error {
 			Message: "Не смогли добавить сообщение",
 		})
 	}
-	messages = append(messages, message)
+	message.ID = nextID
+	nextID++
+
+	messages[message.ID] = message
 	return c.JSON(http.StatusOK, Response{
 		Status:  "Dobavil",
 		Message: "Message dobavlen",
 	})
 }
+
 func DeleteHandler(c echo.Context) error {
-	if len(messages) == 0 {
-		return c.JSON(http.StatusNotFound, Response{
-			Status:  "error",
-			Message: "No messages to delete",
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, Response{
+			Status:  "Error",
+			Message: "Не верный айди",
 		})
 	}
-	lastmessage := messages[len(messages)-1]
-	messages = messages[:len(messages)-1]
+	if _, i := messages[id]; !i {
+		return c.JSON(http.StatusBadRequest, Response{
+			Status:  "Error",
+			Message: "Нет сообщения",
+		})
+	}
+	delete(messages, id)
+	return c.JSON(http.StatusOK, Response{
+		Status:  "Success",
+		Message: "Сообщение удалено",
+	})
+}
+
+func PatchHandler(c echo.Context) error {
+
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, Response{
+			Status:  "Error",
+			Message: "Не верный айди",
+		})
+	}
+
+	var updateMessage Message
+	// Bind переводит Json в сообщение
+	if err := c.Bind(&updateMessage); err != nil {
+		return c.JSON(http.StatusBadRequest, Response{
+			Status:  "Error",
+			Message: "Не смогли обновить сообщение",
+		})
+	}
+
+	if _, i := messages[id]; !i {
+		return c.JSON(http.StatusBadRequest, Response{
+			Status:  "Error",
+			Message: "Нет сообщения",
+		})
+	}
+
+	updateMessage.ID = id
+	messages[id] = updateMessage
 
 	return c.JSON(http.StatusOK, Response{
-		Status:  "Vse ok",
-		Message: "Ybral message:" + lastmessage.Text,
+		Status:  "Success",
+		Message: "Сообщение обновлено",
 	})
 }
 
 func main() {
+
 	e := echo.New()
 	e.GET("/messages", GetHandler)
 	e.POST("/messages", PostHandler)
-	e.DELETE("/messages", DeleteHandler)
+	e.DELETE("/messages/:id", DeleteHandler)
+	e.PATCH("/messages/:id", PatchHandler)
 
 	e.Start("localhost:8080")
+
 }
